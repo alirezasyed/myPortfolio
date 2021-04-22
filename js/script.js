b2e959efa9d32aa6fd27f48e8832d1faf7395b5c
alirezasyed <codingchallenge2020@yahoo.com>
2021-04-21 21:30:44 -0700

    ajoute le code de particules et une image

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-// $(document).scroll(function() {
-//     $('.navbar').toggleClass('scrolled', $(this).scrollTop() > $('.navbar').height());
-// });
+$(document).scroll(function() {
+    $('.navbar').toggleClass('scrolled', $(this).scrollTop() > $('.navbar').height());
+});
 
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
@@ -79,74 +79,3 @@ class Particle {
         this.draw();
     }
 }
-
-// create particle array
-function init() {
-    particlesArray = [];
-    let numberOfParticles = (canvas.height * canvas.width) / 5000;
-    for (let i = 0; i < numberOfParticles * .5; i++) {
-        let size = (Math.random() * 5) + 1;
-        let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2);
-        let y = (Math.random() * ((innerHeight - size * 2) - (size * 2)) + size * 2);
-        let directionX = (Math.random() * 5) - 2.5;
-        let directionY = (Math.random() * 5) - 2.5;
-        let color = '#8C5523';
-
-        particlesArray.push(new Particle(x, y, directionX, directionY, size, color));
-    }
-}
-
-// also we can decease the speed of  particles while resizing 
-// check if particles are close enough to draw line between them
-function connect() {
-    let opacityValue = 1;
-    for (let a = 0; a < particlesArray.length; a++) {
-        for (let b = a; b < particlesArray.length; b++) {
-            let distance = ((particlesArray[a].x - particlesArray[b].x) *
-                    (particlesArray[a].x - particlesArray[b].x)) +
-                ((particlesArray[a].y - particlesArray[b].y) *
-                    (particlesArray[a].y - particlesArray[b].y));
-            if (distance < (canvas.width / .5) * (canvas.height / .5)) {
-                opacityValue = 1 - (distance / 20000);
-                ctx.strokeStyle = 'rgba(140, 85, 31, ' + opacityValue + ')';
-                ctx.lineWidth = 1;
-                ctx.beginPath();
-                ctx.moveTo(particlesArray[a].x, particlesArray[a].y);
-                ctx.lineTo(particlesArray[b].x, particlesArray[b].y);
-                ctx.stroke();
-            }
-        }
-    }
-}
-
-// animation loop
-function animate() {
-    requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, innerWidth, innerHeight);
-
-    for (let i = 0; i < particlesArray.length; i++) {
-        particlesArray[i].update();
-    }
-    connect();
-}
-
-// resize event
-window.addEventListener('resize',
-    function() {
-        canvas.width = innerWidth;
-        canvas.height = innerHeight;
-        mouse.radius = ((canvas.height / 80) * (canvas.height / 80));
-        init();
-    }
-);
-
-// mouse out event 
-window.addEventListener('mouseout',
-    function() {
-        mouse.x = undefined;
-        mouse.y = undefined;
-    }
-)
-
-init();
-animate();
